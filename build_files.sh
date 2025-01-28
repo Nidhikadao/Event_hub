3a569a47d4e2995eed830c8cf08e61f5fc8b2055
Nidhikadao <nidhikadao06@gmail.com>
2025-01-28 17:10:47 +0530

    some changes

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+# Create a virtual environment if it doesn't exist
+python3.9 -m venv venv
+# Activate the virtual environment
+source venv/bin/activate
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# Collect static files
+python manage.py collectstatic --noinput
+echo "BUILD END"
