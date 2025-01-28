bad10ea60b8092c5487505a9314021c145a77770
Nidhikadao <nidhikadao06@gmail.com>
2025-01-28 21:50:00 +0530

    static build added

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,2 @@
-# Create a virtual environment if it doesn't exist
-python3.9 -m venv venv
-# Activate the virtual environment
-source venv/bin/activate
-# Install dependencies
 pip install -r requirements.txt
-# Collect static files
-python manage.py collectstatic --noinput
-echo "BUILD END"
+python3.9 manage.py collectstatic
